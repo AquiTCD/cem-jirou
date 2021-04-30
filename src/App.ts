4b312619347295a13e22739c14d49256863d9e9f
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2021-04-30 13:21:12 +0900

    chore: 動作するサンプルを修正

--- src/App.ts
+++ src/App.ts
@@ -1,13 +1,66 @@
 import dotenv from 'dotenv'
-import { App } from '@slack/bolt'
+import { App, LogLevel } from '@slack/bolt'
 
 import { SampleFormModal } from './Views'
 
 dotenv.config()
 
 const app = new App({
+  logLevel:
+    process.env.NODE_ENV === 'production' ? LogLevel.INFO : LogLevel.DEBUG,
+  socketMode: true,
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+})
+
+// sample
+app.message('こんにちは', async ({ message, say }) => {
+  // @ts-expect-error: Bolt v3.4 で修正される予定
+  // https://github.com/slackapi/bolt-js/issues/826
+  await say(`:wave: こんにちは <@${message.user}>！`)
+})
+
+// sample
+app.shortcut('socket-mode-shortcut', async ({ ack, body, client }) => {
+  await ack()
+  await client.views.open({
+    trigger_id: body.trigger_id,
+    view: {
+      type: 'modal',
+      callback_id: 'modal-id',
+      title: {
+        type: 'plain_text',
+        text: 'タスク登録',
+      },
+      submit: {
+        type: 'plain_text',
+        text: '送信',
+      },
+      close: {
+        type: 'plain_text',
+        text: 'キャンセル',
+      },
+      blocks: [
+        {
+          type: 'input',
+          block_id: 'input-task',
+          element: {
+            type: 'plain_text_input',
+            action_id: 'input',
+            multiline: true,
+            placeholder: {
+              type: 'plain_text',
+              text: 'タスクの詳細・期限などを書いてください',
+            },
+          },
+          label: {
+            type: 'plain_text',
+            text: 'タスク',
+          },
+        },
+      ],
+    },
+  })
 })
 
 app.command('/sample_form_modal', async ({ ack, body, context, payload }) => {
@@ -52,7 +105,7 @@ app.view('send_form', async ({ ack, body, context, view }) => {
 })
 
 const run = async () => {
-  await app.start(process.env.PORT || 3000)
+  await app.start(Number(process.env.PORT) || 3000)
 
   console.log('⚡️ Bolt app is running!')
 }
